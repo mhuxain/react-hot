f926d3ed9930fdf0d3c7c17d3e29b3e2ae505bac
huxain <h.xain@yahoo.com>
2016-10-03 16:14:15 +0700

    configured simple hot module load

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
+  
+	constructor() {
+		super();
+		this.state = {
+			todoList : ["apples", "oranges", "grapes", "bananas"]
+		}
+	}
+
+	create(todo) {
+		this.state.todoList.push(todo)
+		this.setState(this.state);
+	}
+
   render() {
+    
     return (
       <div>
-      React test example.
+
+      <input type="text" ref="newTodo"/>
+      <button onClick={(e) => {this.create(this.refs.newTodo.value); this.refs.newTodo.value = "";}}>Create</button>
+      <br />
+      <ol>
+      	{
+      		this.state.todoList.map((todo, i) => (
+      			<li key={i}>{todo}<input type="button" onClick={() => this.delete(todo) } value="X" />
+      			</li>
+
+      			))
+      	}
+      </ol>
+      React test example list.
       </div>
     );
   }
